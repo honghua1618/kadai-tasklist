f929b013fd11b497c537bb746c483f967fe0e67a
hiroto.tagoori <tagori@icloud.com>
2019-08-02 10:41:25 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
   @tasks = Task.all
   end
 
   def show
-  @task = Task.find(params[:id])
+  set_task
   end
 
   def new
@@ -24,11 +26,11 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    set_task
   end
 
   def update
-    @task = Task.find(params[:id])
+    set_task
 
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
@@ -48,9 +50,12 @@ class TasksController < ApplicationController
   end
   
   private
-  def task_params
-    params.require(:task).permit(:content)
+  
+  def set_task
+  @task = Task.find(params[:id])
   end
   
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
 end
-
